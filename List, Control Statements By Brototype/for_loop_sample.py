749458d47a9e5667da9032e701f7945572bc6d4a
MUHAMMED AFTHAL K <muhammedafthal786gmail.com@MuhammethalsAir.lan>
2025-05-08 16:00:49 +0530

    Just_Completed_While&For Loop

---
+++ List, Control Statements By Brototype/for_loop_sample.py
@@ -0,0 +1,15 @@
+
+files = ["KGF.mp4", "Empuraan.mp4", "Banglore Days.mp4"]
+
+# This is the for-loop in python. And we can filter them with the given method.
+for x in files:
+    print(x)
+    print(x[0:-4])
+
+# This is for count in for-loop.
+for i in range(10):
+    print(i)
+
+# This way we can print multiplication. Eg: multiplication of 5.
+for n in range(5, 55, 5):
+    print(n)
